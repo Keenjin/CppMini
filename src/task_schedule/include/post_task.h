50f455859bebe939f3789f05716ad6e59324c89d
keen <keen@douyu.tv>
2020-09-06 11:13:16 +0800

    导出头文件给外部使用

---
+++ src/task_schedule/include/post_task.h
@@ -0,0 +1,79 @@
+#pragma once
+#include "task_def.h"
+#include "bind.h"
+
+namespace task_schedule {
+	namespace internal {
+		template <typename ReturnType>
+		void ReturnAsParamAdapter(OnceCallback<ReturnType()> func,
+			std::unique_ptr<ReturnType>* result) {
+			result->reset(new ReturnType(std::move(func).Run()));
+		}
+
+		template <typename TaskReturnType>
+		void ReplyAdapter(OnceCallback<void(TaskReturnType)> callback,
+			std::unique_ptr<TaskReturnType>* result) {
+			assert(result->get());
+			std::move(callback).Run(std::move(**result));
+		}
+	}
+
+	// 相关任务类型必须经过注册，才能使用。默认不注册，减少不必要的资源消耗
+	bool Register(TaskThreadType type);
+	void UnRegister(TaskThreadType type);
+	void UnRegisterAll();
+
+	// 适合一些异步任务，不需要回复
+	bool PostTask(
+		TaskThreadType type,
+		OnceClosure task, 
+		TaskPriority priority = TaskPriority::NORMAL);
+
+	// 适合一些异步任务，回复任务执行完毕
+	bool PostTaskAndReply(
+		TaskThreadType type, 
+		OnceClosure task, 
+		OnceClosure reply, 
+		TaskPriority priority = TaskPriority::NORMAL);
+	bool PostTaskAndReply(
+		TaskThreadType task_thread_type, OnceClosure task, 
+		TaskThreadType reply_thread_type, OnceClosure reply, 
+		TaskPriority task_priority = TaskPriority::NORMAL, 
+		TaskPriority reply_priority = TaskPriority::NORMAL);
+
+	// 发一个任务，执行结果通过返回值回传给reply，即：reply的输入参数，就是前面执行任务的返回结果
+	template<typename TaskReturnType>
+	bool PostTaskAndReplyWithResult(
+		TaskThreadType type,
+		OnceCallback<TaskReturnType()> task,
+		OnceCallback<void(TaskReturnType)> reply,
+		TaskPriority priority = TaskPriority::NORMAL) {
+		auto* result = new std::unique_ptr<TaskReturnType>();
+		return PostTaskAndReply(type,
+			BindOnce(&internal::ReturnAsParamAdapter<TaskReturnType>, std::move(task), result),
+			BindOnce(&internal::ReplyAdapter<TaskReturnType>, std::move(reply), Owned(result)), 
+			priority);
+	}
+
+	template<typename TaskReturnType>
+	bool PostTaskAndReplyWithResult(
+		TaskThreadType task_thread_type,
+		OnceCallback<TaskReturnType()> task,
+		TaskThreadType reply_thread_type,
+		OnceCallback<void(TaskReturnType)> reply,
+		TaskPriority task_priority = TaskPriority::NORMAL,
+		TaskPriority reply_priority = TaskPriority::NORMAL) {
+		auto* result = new std::unique_ptr<TaskReturnType>();
+		return PostTaskAndReply(task_thread_type,
+			BindOnce(&internal::ReturnAsParamAdapter<TaskReturnType>, std::move(task), result),
+			reply_thread_type,
+			BindOnce(&internal::ReplyAdapter<TaskReturnType>, std::move(reply), Owned(result)),
+			task_priority, reply_priority);
+	}
+
+	// 带上下文的任务
+
+
+	// 当没有消息循环时，需要加上这个，才能正常使用UI的线程模型
+	void RunUILoopForTest();
+}
---
+++ src/task_schedule/include/task_def.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <stdint.h>
+
+namespace task_schedule {
+
+	enum class TaskThreadType : int8_t {
+		UI = 0,			// 任务被执行在UI线程
+		Background,		// 任务被执行在进程内固定的后台线程
+		Pool,			// 任务被执行在一个线程池中，线程个数会根据任务量和CPU空闲量动态计算，个数范围：1~255
+		// Target,			// 任务被执行在任意目标线程，需要给定目标线程的id，任务会自动切换到目标线程异步执行  -- todo
+
+		Max
+	};
+
+	enum class TaskPriority : int8_t {
+		LOWEST = 0,
+		LOW,
+		NORMAL,
+		HIGH,
+		HIGHEST
+	};
+
+}
